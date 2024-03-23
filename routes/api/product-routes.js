d8b390d265ba734ac6b63200283b853bfd51ae16
Greg Yerkes <gyerkes@rajant.com>
2024-03-23 16:08:52 -0400

    added routing code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product
+    .findAll({
+      include: [Category, Tag]
+    })
+    .then(productData => res.json(productData))
+    .catch(err => {
+      console.log(error);
+      res.status(500).json(err);
+    });
+
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product
+  .findOne({
+    where: { id: req.params.id },
+    include: [Category, Tag]
+  })
+  .then(productData => {
+    if (!productData) {
+      res.status(404).json({ message: "Product not found with this id." });
+      return;
+    }
+    res.json(productData)
+  })
+  .catch(err => {
+    console.log(error);
+    res.status(500).json(err);
+  });
+  
 });
 
 // create new product
@@ -94,6 +121,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product
+    .destroy({
+      where: { id: req.params.id }
+    })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'Product not found with this id.' });
+        return;
+      }
+      res.json({ message: 'Product deleted.' });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
